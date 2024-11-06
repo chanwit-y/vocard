1e6aca16993cf9606b575f160db88d9d1e541990
cy <chanwit.yimneam@hotmail.com>
2024-11-06 18:39:07 +0700

    layout

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps<{
+	title: string
+}>()
+
+</script>
+
+<template>
+	<div>
+		<h1>{{ props.title }}</h1>
+		<p>Welcome to my Nuxt app!</p>
+		<slot />
+	</div>
+</template>
