e4e6edfb13044020014018f1f19ef5d9ea59d551
cy <chanwit.yimneam@hotmail.com>
2024-11-29 18:52:28 +0700

    enhance Card component layout with overlay styling and scoped styles

--- components/Card.vue
+++ components/Card.vue
@@ -8,15 +8,19 @@ const target = ref<HTMLElement | null>(null);
 
 <template>
 	<div ref="container" class="container">
-		<div ref="target">
-			<p>Hi</p>
+		<div ref="target" class="overlay">
+			<p text-black>Hi</p>
 		</div>
 	</div>
 </template>
 
-<style>
+<style scoped>
 .container {
 	@apply: flex relative border border-style-dashed border-amber rounded-md items-center justify-center overflow-hidden;
+	height: 10rem;
 	transform: all 0.2s ease-in-out;
+}
+.overlay {
+	@apply: absolute rounded-md bg-white w-full h-full flex top-0 left-0 items-center justify-center; 
 }
 </style>
