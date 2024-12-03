2bd259bc71f30ccac4b21a6afd77a3d269d0ae24
cy <chanwit.yimneam@hotmail.com>
2024-12-03 10:23:43 +0700

    refactor index page layout; add navigation and update Card component positioning

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
 import Card from '../components/Card.vue';
-
 </script>
 
 <template>
-	<div>
-		<Card />
+	<div h-screen p-4 grid="~ grid content-around cols-[1fr] rows-[1fr_1fr]">
+		<nav flex justify-between>
+			<div w-8 h-8 i-material-symbols-menu-rounded></div>
+			<!-- i-material-symbols-dark-mode-outline-rounded -->
+			<div w-8 h-8 i-material-symbols-light-mode-outline></div>
+		</nav>
+		<Card mb-8 self-end />
 	</div>
 </template>
-
-<style scoped>
-	div {
-		@apply: p-4;
-	}
-</style>
