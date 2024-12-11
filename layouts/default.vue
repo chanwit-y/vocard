6f12dc9d9e4479b9ad5dc23e30794be118c7f483
cy <chanwit.yimneam@hotmail.com>
2024-12-11 16:42:22 +0700

    update layout responsiveness in default.vue; adjust screen handling for mobile and desktop views

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,11 +5,11 @@ const { width, height } = useWindowSize()
 </script>
 
 <template>
-	<div v-if="width <= 430" h-screen  grid="~ grid content-around cols-[1fr] rows-[1fr_1fr]">
+	<div v-if="width <= 430" h-screen-md  grid="~ grid content-around cols-[1fr] rows-[1fr_1fr]">
 		<NavBar />
 		<slot />
 	</div>
-	<div v-if="width > 430" h-screen flex justify-center items-center >
+	<div v-if="width > 430"  flex justify-center items-center >
 		<span tracking-wider text-gray-400>This app support just mobile screen only~😵‍💫</span>
 	</div>
 </template>
